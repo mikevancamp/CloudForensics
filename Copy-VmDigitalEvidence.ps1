5a8288faa0f03aae161f7ee27c19a6f7f4463e75
mikevancamp <126270961+mikevancamp@users.noreply.github.com>
2023-03-08 16:00:32 +0100

    Rewrite Copy-VmDigitalEvidence for larger scope

--- Copy-VmDigitalEvidence.ps1
+++ Copy-VmDigitalEvidence.ps1
@@ -1,11 +1,10 @@
 # Source: https://github.com/mspnp/solution-architectures/blob/master/forensics/Copy-VmDigitalEvidence.ps1
-# This code has been tweaked for the cloud research paper
+# This code has been modified for our cloud research paper
 
 Param(
-    # Add when there are multiple resource groups, for this lab only one resource group is used
-    # [Parameter(Mandatory = $true)]
-    # [string]
-    # $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
 
     [Parameter(Mandatory = $true)]
     [string]
@@ -13,18 +12,16 @@ Param(
 )
 
 ######################################### SOC Constants #####################################
-$subId = '9b4e5031-850b-4baf-9fd9-760e8b20060f'     # Subscription ID
-$ResourceGroupName = 'cloud-forensics'              # Resource Group Name
-$destSAblob = 'socdisks'                            # Storage account for BLOB (immutable)
-$destSAfile = 'soctempdisks'                        # Storage account for FILE share
+$subId = '<your_subscription_id>'                   # Subscription ID
+$socResourceGroup = 'SOC'                           # Resource Group Name
+$destSAblob = 'socsecret'                           # Storage account for BLOB (immutable)
+$destSAfile = 'socsecret'                           # Storage account for FILE share
 $destTempShare = 'disks'                            # The temporary file share mounted on the hybrid worker
 $destSAContainer = 'disks'                          # Container name of $destSAblob
-$destKV = 'basicvault'                              # Key vault name to store a copy of the hashes
+$destKV = 'KV-SOC'                                  # Key vault name to store a copy of the hashes
 
 $targetLinuxDir = "/mount/$destSAfile/$destTempShare"             # Name dir where file share is mounted on
 $snapshotPrefix = (Get-Date).toString('yyyyMMddHHmm') # The prefix of the snapshot to be created
-
-Write-Output "Running on Hybrid Worker"
 
 ############################# Authenticating to Azure #############################
 Disable-AzContextAutosave -Scope Process
@@ -37,22 +34,22 @@ Write-Output "#################################"
 
 Get-AzSubscription -SubscriptionId $subId | Set-AzContext
 
-$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VirtualMachineName
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VirtualMachineName # Get target VM
 
 $osdisk = Get-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $vm.StorageProfile.OsDisk.Name
 $snapshot = New-AzSnapshotConfig -SourceUri $osdisk.id -CreateOption Copy -Location $vm.location
 $snapshotName = $snapshotPrefix + "-" + $osdisk.name.Replace("_", "-")
 
-New-AzSnapshot -ResourceGroupName $ResourceGroupName -Snapshot $snapshot -SnapshotName $snapshotName
+New-AzSnapshot -ResourceGroupName $socResourceGroup -Snapshot $snapshot -SnapshotName $snapshotName # Save to the SOC resource group
 
 ##################### Copy the OS snapshot from source to file share and blob container ########################
 Write-Output "#################################"
 Write-Output "Copy the OS snapshot from source to file share and blob container"
 Write-Output "#################################"
 
-$snapSasUrl = Grant-AzSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $snapshotName -DurationInSecond 7200 -Access Read
-$targetStorageContextBlob = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAblob).Context
-$targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAfile).Context
+$snapSasUrl = Grant-AzSnapshotAccess -ResourceGroupName $socResourceGroup -SnapshotName $snapshotName -DurationInSecond 7200 -Access Read
+$targetStorageContextBlob = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAblob).Context
+$targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAfile).Context
 
 Write-Output "Start Copying Blob $snapshotName"
 Start-AzStorageBlobCopy -AbsoluteUri $snapSasUrl.AccessSAS -DestContainer $destSAContainer -DestContext $targetStorageContextBlob -DestBlob "$snapshotName.vhd" -Force
@@ -70,17 +67,17 @@ $hashfull = Invoke-Expression -Command "sha256sum $diskpath"
 $hash = $hashfull.split(" ")[0]
 Write-Output "Computed SHA-256: $hash"
 
-# #################### Copy the OS BEK to the SOC Key Vault  ###################################
-# $BEKurl = $osdisk.EncryptionSettingsCollection.EncryptionSettings.DiskEncryptionKey.SecretUrl
-# Write-Output "#################################"
-# Write-Output "OS Disk Encryption Secret URL: $BEKurl"
-# Write-Output "#################################"
-# if ($BEKurl) {
-#     $sourcekv = $BEKurl.split("/")
-#     $BEK = Get-AzKeyVaultSecret -VaultName $sourcekv[2].split(".")[0] -Name $sourcekv[4] -Version $sourcekv[5]
-#     Write-Output "Key value: $BEK"
-#     Set-AzKeyVaultSecret -VaultName $destKV -Name $snapshotName -SecretValue $BEK.SecretValue -ContentType "BEK" -Tag $BEK.Tags
-# }
+# # #################### Copy the OS BEK to the SOC Key Vault  ###################################
+# # $BEKurl = $osdisk.EncryptionSettingsCollection.EncryptionSettings.DiskEncryptionKey.SecretUrl
+# # Write-Output "#################################"
+# # Write-Output "OS Disk Encryption Secret URL: $BEKurl"
+# # Write-Output "#################################"
+# # if ($BEKurl) {
+# #     $sourcekv = $BEKurl.split("/")
+# #     $BEK = Get-AzKeyVaultSecret -VaultName $sourcekv[2].split(".")[0] -Name $sourcekv[4] -Version $sourcekv[5]
+# #     Write-Output "Key value: $BEK"
+# #     Set-AzKeyVaultSecret -VaultName $destKV -Name $snapshotName -SecretValue $BEK.SecretValue -ContentType "BEK" -Tag $BEK.Tags
+# # }
 
 ######## Copy the OS disk hash value in key vault and delete disk in file share ##################
 Write-Output "#################################"
@@ -88,7 +85,7 @@ Write-Output "OS disk - Put hash value in Key Vault"
 Write-Output "#################################"
 $secret = ConvertTo-SecureString -String $hash -AsPlainText -Force
 Set-AzKeyVaultSecret -VaultName $destKV -Name "$snapshotName-sha256" -SecretValue $secret -ContentType "text/plain"
-$targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAfile).Context
+$targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAfile).Context
 Remove-AzStorageFile -ShareName $destTempShare -Path $snapshotName -Context $targetStorageContextFile
 
 ############################ Snapshot the data disks, store hash and BEK #####################
@@ -100,15 +97,15 @@ foreach ($dataDisk in $vm.StorageProfile.DataDisks) {
     $dsnapshotName = $snapshotPrefix + "-" + $ddisk.name.Replace("_", "-")
     $dsnapshotList += $dsnapshotName
     Write-Output "Snapshot data disk name: $dsnapshotName"
-    New-AzSnapshot -ResourceGroupName $ResourceGroupName -Snapshot $dsnapshot -SnapshotName $dsnapshotName
+    New-AzSnapshot -ResourceGroupName $socResourceGroup -Snapshot $dsnapshot -SnapshotName $dsnapshotName
 
     Write-Output "#################################"
     Write-Output "Copy the Data Disk $dsnapshotName snapshot from source to blob container"
     Write-Output "#################################"
 
-    $dsnapSasUrl = Grant-AzSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $dsnapshotName -DurationInSecond 72000 -Access Read
-    $targetStorageContextBlob = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAblob).Context
-    $targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAfile).Context
+    $dsnapSasUrl = Grant-AzSnapshotAccess -ResourceGroupName $socResourceGroup -SnapshotName $dsnapshotName -DurationInSecond 72000 -Access Read
+    $targetStorageContextBlob = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAblob).Context
+    $targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAfile).Context
 
     Write-Output "Start Copying Blob $dsnapshotName"
     Start-AzStorageBlobCopy -AbsoluteUri $dsnapSasUrl.AccessSAS -DestContainer $destSAContainer -DestContext $targetStorageContextBlob -DestBlob "$dsnapshotName.vhd" -Force
@@ -149,7 +146,7 @@ foreach ($dataDisk in $vm.StorageProfile.DataDisks) {
     Write-Output "#################################"
     $Secret = ConvertTo-SecureString -String $dhash -AsPlainText -Force
     Set-AzKeyVaultSecret -VaultName $destKV -Name "$dsnapshotName-sha256" -SecretValue $Secret -ContentType "text/plain"
-    $targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destSAfile).Context
+    $targetStorageContextFile = (Get-AzStorageAccount -ResourceGroupName $socResourceGroup -Name $destSAfile).Context
     Remove-AzStorageFile -ShareName $destTempShare -Path $dsnapshotName -Context $targetStorageContextFile
 
 }
@@ -160,9 +157,9 @@ foreach ($dsnapshotName in $dsnapshotList) {
     Get-AzStorageBlobCopyState -Blob "$dsnapshotName.vhd" -Container $destSAContainer -Context $targetStorageContextBlob -WaitForComplete
 }
 
-Revoke-AzSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $snapshotName
-Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $snapshotname -Force
+Revoke-AzSnapshotAccess -ResourceGroupName $socResourceGroup -SnapshotName $snapshotName
+Remove-AzSnapshot -ResourceGroupName $socResourceGroup -SnapshotName $snapshotname -Force
 foreach ($dsnapshotName in $dsnapshotList) {
-    Revoke-AzSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $dsnapshotName
-    Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $dsnapshotname -Force
+    Revoke-AzSnapshotAccess -ResourceGroupName $socResourceGroup -SnapshotName $dsnapshotName
+    Remove-AzSnapshot -ResourceGroupName $socResourceGroup -SnapshotName $dsnapshotname -Force
 }
